36f8ded40b9986a45e461db60328d6a4508e79ea
ekrambd <ekramhossainekram28@gmail.com>
2023-06-28 22:06:49 +0600

    first_commit

---
+++ database/migrations/2023_06_28_123607_create_passwords_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('user_name');
+            $table->string('general_password');
+            $table->string('encrypt_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passwords');
+    }
+};
