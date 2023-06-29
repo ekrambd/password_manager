5501e2eeb7d8d69f08682f99652f9d8f09513ba5
ekrambd <ekramhossainekram28@gmail.com>
2023-06-29 21:29:24 +0600

    mastering

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'indexPage']);
+
+//categories
+Route::resource('categories', CategoryController::class);
